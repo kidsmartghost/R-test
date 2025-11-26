f999c789b4f43a9361805b45070aec36662177bf
kidsmartghost <42663458+kidsmartghost@users.noreply.github.com>
2025-11-26 05:13:14 +0000

    Update config_management.R

--- config_management.R
+++ config_management.R
@@ -2,7 +2,7 @@
 load_config <- function() {
   # 从环境变量获取配置（云平台推荐方式）
   config <- list(
-    base_dir <- getwd(),
+    base_dir <- "./",
     db_type = Sys.getenv("DB_TYPE", "sqlite")
     
   )
@@ -10,3 +10,4 @@ load_config <- function() {
   return(config)
 
 }
+
