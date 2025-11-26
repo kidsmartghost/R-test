1ddca93ae40b49256bee88bcc6c6d2d4b5abdf7a
kidsmartghost <42663458+kidsmartghost@users.noreply.github.com>
2025-11-26 05:05:20 +0000

    Update config_management.R

--- config_management.R
+++ config_management.R
@@ -2,10 +2,11 @@
 load_config <- function() {
   # 从环境变量获取配置（云平台推荐方式）
   config <- list(
-    base_dir <- Sys.getenv("YTwork","C:/Users/Yubin.Luo/Desktop/R Shiny/R test1"),
+    base_dir <- getwd(),
     db_type = Sys.getenv("DB_TYPE", "sqlite")
     
   )
   
   return(config)
+
 }
